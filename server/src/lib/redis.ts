5d22507798f4ec92de0535f1557bcb8f7eb7ba76
Sagar Kapoor <142504626+SagarKapoorin@users.noreply.github.com>
2025-12-21 15:01:09 +0530

    Update redis.ts

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -2,6 +2,7 @@ import { Redis } from 'ioredis';
 import { env } from '../config/env.js';
 
 const redisClient = new Redis(env.redisUrl, {
+  lazyConnect:true
 });
 
 redisClient.on('connect', () => {
